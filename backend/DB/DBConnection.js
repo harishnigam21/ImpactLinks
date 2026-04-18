1b2a15555007fdbbc4f281dc36a7eeab55d0f81e
Harish Nigam <harishnigam21@gmail.com>
2026-04-18 14:26:05 +0530

    Create DBConnection.js
    Function that connect the Database

---
+++ backend/DB/DBConnection.js
@@ -0,0 +1,25 @@
+//mongoose package, this will help us to interact with our mongo DB
+import mongoose from "mongoose";
+import { envList } from "../envConfig.js";
+
+const connectDB = async () => {
+  try {
+    console.log("mongoose is connecting ....");
+    //connecting with our DB with the help of connection string
+    await mongoose.connect(envList.DB_STRING, {
+      serverSelectionTimeoutMS: 5000, // setting timeout so that it does not lead to finite timeout.
+    });
+    // this gives the info of our DB in object and after getting this we can handle DB error and other thing
+    const db = mongoose.connection;
+    //on error
+    db.on("error", () => {
+      console.error("mongoose failed to connect");
+      return;
+    });
+    //otherwise
+    console.log("mongoose got connected successfully.");
+  } catch (error) {
+    console.error("Caught an error while connecting mongoose,", error.message);
+  }
+};
+export default connectDB;
